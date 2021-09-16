1d1e2ec6c3bd486feba3a2ac5baf9524ddf4c347
Devin Cameron <devincameron@quickenloans.com>
2021-09-16 10:14:19 -0400

    first commit

---
+++ CoffeeShopLabFixed/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+using CoffeeShopLabFixed.Models;
+using Dapper;
+using Dapper.Contrib.Extensions;
+using MySql.Data.MySqlClient;
+
+namespace CoffeeShopLabFixed.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            MySqlConnection db = new MySqlConnection("Server=localhost;Database=coffeeshop;Uid=root;Password=abc123");
+            List<Product> prods = db.GetAll<Product>().ToList();
+
+            return View(prods);
+        }
+        public IActionResult Detail(int pr)
+        {
+            MySqlConnection db = new MySqlConnection("Server=localhost;Database=coffeeshop;Uid=root;Password=abc123");
+            Product prod = db.Get<Product>(pr);
+            return View(prod);
+        }
+    }
+}
---
+++ CoffeeShopLabFixed/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper.Contrib.Extensions;
+using Dapper;
+using MySql.Data.MySqlClient;
+using CoffeeShopLabFixed.Models;
+
+namespace CoffeeShopLabFixed.Models
+{
+    
+        [Table("Products")]
+        public class Product
+        {
+            [Key]
+            public int productID { get; set; }
+            public string productNAME { get; set; }
+            public string productDESCRIPTION { get; set; }
+            public string productPRICE { get; set; }
+            public string productCATEGORY { get; set; }
+        }
+}
